49e9e60ba7716b93ba88115e62560c9d2189d2ef
Joao Carnevalli <joaovictor.carnevalli@gmail.com>
2026-09-18 15:37:42 -0300

    atualiza

--- 1 - JavaScript Inicial/7 - script.js
+++ 1 - JavaScript Inicial/7 - script.js
@@ -5,3 +5,15 @@ const reembolsos = [
   { id: 4, funcionario: "Roberto", valor: 800, status: "Rejeitado" },
   { id: 5, funcionario: "Aline", valor: 600, status: "Aprovado" }
 ];
+
+function processar() {
+  const aprovados = reembolsos.filter(item => item.status === "Aprovado");
+  const listaHTML = aprovados.map(item => {
+    return `<p>Funcionário: ${item.funcionario} - R$ ${item.valor.toFixed(2)}</p>`;
+  });
+  const totalSoma = aprovados.reduce((acumulador, item) => acumulador + item.valor, 0);
+  document.querySelector("#lista-reembolsos").innerHTML = listaHTML.join('');
+  
+  document.querySelector("#total-reembolsos").innerHTML = 
+    `<h3>Total Liberado: R$ ${totalSoma.toFixed(2)}</h3>`;
+}
