b10250eb7a9b90ca7a65c195ca5e514d583aa165
Joao Carnevalli <joaovictor.carnevalli@gmail.com>
2026-09-18 15:27:57 -0300

    nova atualizacao

---
+++ 1 - JavaScript Inicial/7 - script.js
@@ -0,0 +1,7 @@
+const reembolsos = [
+  { id: 1, funcionario: "Carlos", valor: 350, status: "Aprovado" },
+  { id: 2, funcionario: "Mariana", valor: 1200, status: "Pendente" },
+  { id: 3, funcionario: "Fernanda", valor: 450, status: "Aprovado" },
+  { id: 4, funcionario: "Roberto", valor: 800, status: "Rejeitado" },
+  { id: 5, funcionario: "Aline", valor: 600, status: "Aprovado" }
+];
